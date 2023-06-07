50fbab88fa41fca556ccc9048f2b3aad7d585d9a
Milosz Lewandowski <113758913+yerbaMatte@users.noreply.github.com>
2023-06-07 23:47:06 +0200

    Frequency algorithm

--- public/index.js
+++ public/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log('ss');
+function sameFrequency(int1, int2) {
+    const string1 = int1.toString().split('');
+    const string2 = int2.toString().split('');
+    if (string1.length !== string2.length)
+        return false;
+    //                          index signature
+    const frequencyObject1 = {};
+    for (let char of string1) {
+        frequencyObject1[char] = (frequencyObject1[char] || 0) + 1;
+    }
+    for (let char of string2) {
+        if (frequencyObject1[char]) {
+            frequencyObject1[char] -= 1;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(sameFrequency(213, 321));
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('ss');
+function sameFrequency(int1: number, int2: number): any {
+  const string1: string[] = int1.toString().split('');
+  const string2: string[] = int2.toString().split('');
+
+  if (string1.length !== string2.length) return false;
+
+  //                          index signature
+  const frequencyObject1: { [key: string]: number } = {};
+
+  for (let char of string1) {
+    frequencyObject1[char] = (frequencyObject1[char] || 0) + 1;
+  }
+
+  for (let char of string2) {
+    if (frequencyObject1[char]) {
+      frequencyObject1[char] -= 1;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(sameFrequency(213, 321));
