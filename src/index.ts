668bcaa67b4b148ca06980dd3bc4faadcc4b6837
Milosz Lewandowski <113758913+yerbaMatte@users.noreply.github.com>
2023-06-08 16:25:59 +0200

    recursion

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
-function maxSubarraySum(arr: number[], subArr: number) {
-  if (arr.length < subArr) return false;
-
-  let maxSum = 0;
-  let tempSum = 0;
-
-  for (let i = 0; i < subArr; i++) {
-    maxSum += arr[i];
+function countUp(num: number) {
+  if (num > 9) {
+    console.log('I got 10!');
+    return;
   }
 
-  tempSum = maxSum;
-
-  for (let i = subArr; i < arr.length; i++) {
-    tempSum += arr[i] - arr[i - subArr];
-    maxSum = Math.max(tempSum, maxSum);
-  }
-
-  return maxSum;
+  console.log(`Number is not greater than 10! It's ${num}!`);
+  num++;
+  countUp(num);
 }
 
-console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4));
+countUp(0);
