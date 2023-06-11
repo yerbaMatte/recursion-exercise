9b64ff91af689b1666548af4ec8214bc6e268076
Milosz Lewandowski <113758913+yerbaMatte@users.noreply.github.com>
2023-06-11 23:27:07 +0200

    naive string search

--- public/index.js
+++ public/index.js
@@ -1,11 +1,2 @@
 "use strict";
-function countUp(num) {
-    if (num > 9) {
-        console.log('I got 10!');
-        return;
-    }
-    console.log(`Number is not greater than 10! It's ${num}!`);
-    num++;
-    countUp(num);
-}
-countUp(0);
+//Naive substring search
