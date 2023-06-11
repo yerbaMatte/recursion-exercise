de6c8f6eabfc92ec89c7ea34ce69bdde610f7ac3
Milosz Lewandowski <113758913+yerbaMatte@users.noreply.github.com>
2023-06-11 23:25:43 +0200

    naive string search

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-function countUp(num: number) {
-  if (num > 9) {
-    console.log('I got 10!');
-    return;
-  }
-
-  console.log(`Number is not greater than 10! It's ${num}!`);
-  num++;
-  countUp(num);
-}
-
-countUp(0);
+//Naive substring search
