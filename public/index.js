336621607e5ec98ad74abc0894c1be4b554d1778
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-06-14 15:42:28 +0200

    binary search

--- public/index.js
+++ public/index.js
@@ -1,27 +1,7 @@
 "use strict";
-function binarySearch(arr, elem) {
-    //
-    var start = 0;
-    var end = arr.length - 1;
-    var middle = Math.floor((start + end) / 2);
-    console.log(start, middle, end);
-    //
-    while (arr[middle] !== elem && start <= end) {
-        if (elem < arr[middle]) {
-            end = middle - 1;
-        }
-        else {
-            start = middle + 1;
-        }
-        console.log(start, middle, end);
-        middle = Math.floor((start + end) / 2);
-    }
-    if (arr[middle] === elem) {
-        return middle;
-    }
-    return -1;
+function lengthOfLastWord(s) {
+    let x = s.trim().split(' ').slice(-1)[0].length;
+    console.log(x);
+    return x;
 }
-//               L           M               R
-const arrTest = [2, 4, 5, 7, 11, 15, 36, 74, 89];
-const targValue = 11;
-console.log(binarySearch(arrTest, targValue));
+lengthOfLastWord('   fly me   to   the moon  ');
