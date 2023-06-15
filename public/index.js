727af6e93938f936e09661e651c00bc35b3db93b
Milosz Lewandowski <113758913+yerbaMatte@users.noreply.github.com>
2023-06-15 13:46:35 +0200

    BinarySearchTree - DFS / BFS

--- public/index.js
+++ public/index.js
@@ -43,5 +43,66 @@ class BinarySearchTree {
             }
         }
     }
+    find(value) {
+        if (this.root === null)
+            return false;
+        let current = this.root;
+        let found = false;
+        while (!found && current) {
+            if (value < current.value) {
+                current = current.left;
+            }
+            else if (value > current.value) {
+                current = current.right;
+            }
+            else {
+                found = true;
+            }
+        }
+        if (!found)
+            return false;
+        return current;
+    }
+    //Breadth first search
+    BFS() {
+        let node = this.root;
+        const data = [];
+        const queue = [];
+        queue.push(this.root);
+        console.log(queue);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left)
+                queue.push(node.left);
+            if (node.right)
+                queue.push(node.right);
+        }
+    }
+    DFSPreOrder() {
+        const data = [];
+        function traverse(node) {
+            data.push(node.value);
+            if (node.left)
+                traverse(node.left);
+            if (node.right)
+                traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSPostOrder() {
+        const data = [];
+        function traverse(node) {
+            if (node.left)
+                traverse(node.left);
+            if (node.right)
+                traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 const tree = new BinarySearchTree();
+tree.root = new NodeBST(6);
