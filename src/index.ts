1c0f3ae0954749441d695ae75ed93c550a371c31
Milosz Lewandowski <113758913+yerbaMatte@users.noreply.github.com>
2023-06-17 23:30:09 +0200

    recursive way staircase

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// Hash Tables
+// stairs
+
+// const memo: { [key: number]: number } = { 1: 1, 2: 2 };
+
+// var climbStairs = function (n: number): number {
+//   const result = memo[n];
+//   if (result !== undefined) {
+//     return result;
+//   }
+//   const left = climbStairs(n - 2);
+//   memo[n - 2] = left;
+//   const right = climbStairs(n - 1);
+//   memo[n - 1] = right;
+
+//   return left + right;
+// };
